21b1797a0be65b961f951a89396e346c17a3aad5
seoyoungKim <seoyoungkim003@gmail.com>
2024-10-16 22:56:59 +0900

    10/16

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,36 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combination";
+
+
+function Helper(gameTurns) {
+  
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+      currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
+
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+    
+  const activePlayer = Helper(gameTurns);
+  
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ?  'O' : 'X');
+    
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
+      
+      const currentPlayer = Helper(prevTurns);
 
-      if (prevTurns.length > 0 && prevTurns[0].square.player === 'X') {
-        currentPlayer = 'O';
-      }
       const updatedTurns = [ {square: {row: rowIndex, col: colIndex}, player: currentPlayer} , ...prevTurns]
       
-
       return updatedTurns;
     });
   }
